0ad3824f11b90f055962e4687d28265ae7f1d359
Andrew Bernstein <andrewbbernstein@gmail.com>
2015-07-30 15:56:51 -0400

    Added octave functionality and fixed key function

--- tune.js
+++ tune.js
@@ -2,23 +2,37 @@ function Tune(){
 
 	this.scale = []
 	this.key = this.setkey(60)
+	console.log(this.key)
 
 }
 
 Tune.prototype.tune = function(input){
-
+	
+	console.log("KEY!",this.key)
 	var base = 440 * Math.pow(2,(this.key-69)/12)
 
 	var scaleDegree = (input - this.key)%this.scale.length
+	
+	if(scaleDegree < 0){
+		scaleDegree = scaleDegree * -1
+	}
 
 	console.log("base",base)
 	console.log("degree", scaleDegree)
-	return base*this.scale[scaleDegree]
+
+	var pitch = base*this.scale[scaleDegree]
+	
+	var octave = Math.floor((input-this.key)/this.scale.length)
+
+	pitch = pitch*(Math.pow(2,octave))
+
+	return pitch
 
 }
 
 Tune.prototype.setkey = function(key){
-	this.key = key;
+	this.key = key
+	return this.key
 }
 
 Tune.prototype.loadscale = function(name){
