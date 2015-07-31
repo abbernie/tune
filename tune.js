9d9c3dc47ed5a13b0f72628a95a0f37ca1b36a06
Ben Taylor <taylorbf@gmail.com>
2015-07-31 17:21:55 -0400

    output modes

--- tune.js
+++ tune.js
@@ -2,31 +2,74 @@ function Tune(){
 
 	this.scale = []
 	this.key = this.setkey(60)
-	console.log(this.key)
+	this.mode = {
+		output: "frequency",
+		input: "MIDI"
+	};
 
 }
 
-Tune.prototype.tune = function(input){
+Tune.prototype.note = function(input){
+
+	var newvalue;
+
+	if (this.mode.output == "frequency") { 
+		newvalue = this.frequency(input)
+	} else if (this.mode.output == "ratio") { 
+		newvalue = this.ratio(input)
+	} else if (this.mode.output == "MIDI") { 
+		newvalue = this.MIDI(input)
+	} else {
+		newvalue = this.frequency(input)
+	}
 	
-	console.log("KEY!",this.key)
+	return newvalue;
+
+}
+
+Tune.prototype.frequency = function(input) {
+
 	var base = 440 * Math.pow(2,(this.key-69)/12)
 
 	var scaleDegree = (input - this.key)%this.scale.length
-	
-	if(scaleDegree < 0){
-		scaleDegree = scaleDegree * -1
-	}
 
-	console.log("base",base)
-	console.log("degree", scaleDegree)
-
-	var pitch = base*this.scale[scaleDegree]
-	
 	var octave = Math.floor((input-this.key)/this.scale.length)
 
-	pitch = pitch*(Math.pow(2,octave))
+	var freq = base*this.scale[scaleDegree]
+	
+	freq = freq*(Math.pow(2,octave))
 
-	return pitch
+	freq = Math.floor(freq*100000000000)/100000000000
+
+	return freq
+
+}
+
+Tune.prototype.ratio = function(input) {
+
+	// what octave is our input
+	var octave = Math.floor((input-this.key)/this.scale.length)
+
+	// which scale degree (0 - scale length) is our input
+	var scaleDegree = (input - this.key)
+	scaleDegree = ( scaleDegree + 160 ) % this.scale.length
+
+	// what ratio is our input to our key
+	var ratio = Math.pow(2,octave)*this.scale[scaleDegree]
+
+	ratio = Math.floor(ratio*100000000000)/100000000000
+
+	return ratio
+
+}
+
+Tune.prototype.MIDI = function(input) {
+
+	var newvalue = this.frequency(input);
+
+	var n = 69 + 12*Math.log(newvalue/440)/Math.log(2)
+
+	return n
 
 }
 
@@ -38,8 +81,7 @@ Tune.prototype.setkey = function(key){
 Tune.prototype.loadscale = function(name){
 	var freqs = TuningList[name].frequencies
 	this.scale = []
-	for (var i=0;i<freqs.length;i++)
+	for (var i=0;i<freqs.length;i++) {
 		this.scale.push(freqs[i]/freqs[0])
-	this.tune(60)
-	console.log(this.scale)
+	}
 }
